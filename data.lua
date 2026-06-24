c3cbad9aa9038368130b1835dee2856d82e04cf2
IpFruion <lockwood.derrick@gmail.com>
2026-06-23 21:31:57 -0500

    Fixed some groups

--- data.lua
+++ data.lua
@@ -1,5 +1,6 @@
 require("prototypes.data")
 require("prototypes.colors")
+require("prototypes.groups")
 require("prototypes.entities")
 require("prototypes.recipes")
 
@@ -8,40 +9,6 @@ function AddFluid(name)
   AddScienceToFluidRecipe(name)
   AddFluidToScienceRecipe(name)
 end
-
-data:extend({
-  {
-    type = "item-group",
-    name = "science-fluid",
-    icon = "__uncorked__/graphics/icons/science-fluid.png",
-    order = "z-a", -- Where it appears in the tabs
-  },
-})
-data:extend({
-  {
-    type = "item-subgroup",
-    name = "science-fluid-row",
-    group = "science-fluid",
-    order = "a-b",
-  },
-})
-data:extend({
-  {
-    type = "item-subgroup",
-    name = "science-fluid-uncork",
-    group = "intermediate-product", -- The inventory tab this row will appear in
-    order = "g",                  -- Determines if this row is above/below other rows
-  },
-})
-
-data:extend({
-  {
-    type = "item-subgroup",
-    name = "science-fluid-cork",
-    group = "intermediate-product", -- The inventory tab this row will appear in
-    order = "g",                  -- Determines if this row is above/below other rows
-  },
-})
 
 AddFluid("automation-science-pack")
 AddFluid("logistic-science-pack")
---
+++ prototypes/groups.lua
@@ -0,0 +1,32 @@
+data:extend({
+  {
+    type = "item-group",
+    name = "science-fluid",
+    icon = "__uncorked__/graphics/icons/science-fluid.png",
+    order = "z-a",
+  },
+})
+data:extend({
+  {
+    type = "item-subgroup",
+    name = "science-fluid-row",
+    group = "science-fluid",
+    order = "a-b",
+  },
+})
+data:extend({
+  {
+    type = "item-subgroup",
+    name = "science-fluid-cork",
+    group = "intermediate-products",
+    order = "f-a",
+  },
+})
+data:extend({
+  {
+    type = "item-subgroup",
+    name = "science-fluid-uncork",
+    group = "intermediate-products",
+    order = "f-b",
+  },
+})
